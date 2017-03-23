22243ac310b4f527bc78f4b0b759cd54141cf03d
Paul Borawski <paulborawski14@gmail.com>
2017-03-23 14:56:11 -0700

    callbacks

--- callbacks/index.js
+++ callbacks/index.js
@@ -1,1 +1,53 @@
 const fs = require('fs')
+
+/* GET BEARS */
+
+function getBears (filepath, done) {
+  fs.readFile(filepath, function(err, bears) {
+    if (err) return done(err)
+
+    fs.readFile('bears.dictionary', function(err, dict) {
+      if (err) return done(err)
+
+      compareBears(bears, dict)
+    })
+  })
+
+  function compareBears (bears, dict) {
+    dict = dict.toString().split('\n')
+    bears = bears.toString().split('\n').filter(function (bear) {
+      return dict.indexOf(bear) !== -1
+    })
+    done(null, bears)
+  }
+}
+
+getBears('animals.txt', function (err, bears) {
+  console.log(bears)
+})
+
+/* GET CATS */
+
+function getCats (filepath, done) {
+  fs.readFile(filepath, function(err, cats) {
+    if (err) return done(err)
+
+    fs.readFile('cats.dictionary', function(err, dict) {
+      if (err) return done(err)
+
+      compareCats(cats, dict)
+    })
+  })
+
+  function compareCats (cats, dict) {
+    dict = dict.toString().split('\n')
+    cats = cats.toString().split('\n').filter(function (cat) {
+      return dict.indexOf(cat) !== -1
+    })
+    done(null, cats)
+  }
+}
+
+getCats('animals.txt', function (err, cats) {
+  console.log(cats)
+})
---
+++ callbacks/readme.md
@@ -0,0 +1,4 @@
+CALLBACKS
+run node index.js in terminal
+animals.txt is a list of animals
+bears.dictionary and cats.dictionary will check animals.txt to see if those specific animals are inside of the .txt file.
